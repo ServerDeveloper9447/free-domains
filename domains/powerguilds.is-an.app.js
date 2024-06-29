b65e88050a0df618060c2fb48ec85be65b471747
Soumalya Ghosh <86161882+ServerDeveloper9447@users.noreply.github.com>
2024-06-29 20:12:21 +0530

    Create powerguilds.is-an.app.js

---
+++ domains/powerguilds.is-an.app.js
@@ -0,0 +1,12 @@
+addSubDomain({
+  description: 'Hobby guilded bot',
+  domain: 'is-an.app',
+  subdomain: 'powerguilds',
+  owner: {
+    email: 'serverdeveloper@duck.com',
+  },
+  record: {
+    CNAME: 'power-guilds.pages.dev'
+  },
+  proxy: false,
+})
